c756aa2be0b8ed9bc152f0c0c4ec249d575b6813
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-02 10:49:22 -0700

    import routing from app/routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { RidingsoloComponent } from './ridingsolo/ridingsolo.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RidingsoloComponent
   ],
   imports: [
     BrowserModule
