8969b8b01c8af4d0d45a37aebf064d21328a5685
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-02 12:48:38 -0700

    import and route for home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { RidingsoloComponent } from './ridingsolo/ridingsolo.component';
 import { BeautyComponent } from './beauty/beauty.component';
 import { FashionComponent } from './fashion/fashion.component';
 import { FoodComponent } from './food/food.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   {
@@ -26,6 +27,10 @@ const appRoutes: Routes = [
   {
     path: 'food',
     component: FoodComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
