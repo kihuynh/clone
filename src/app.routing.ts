31c4ccbb78046ed25bb2624cb52847d2dd3e3b6d
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-02 11:13:02 -0700

    fix import routing with component

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -1,2 +1,11 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RidingsoloComponent } from './app/ridingsolo/ridingsolo.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: RidingsoloComponent
+  }
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
