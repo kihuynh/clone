0043051d45ce46894d26da2b8623cad94d221b19
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-02 11:20:52 -0700

    move file and fix routing issues

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RidingsoloComponent } from './app/ridingsolo/ridingsolo.component';
+import { RidingsoloComponent } from './ridingsolo/ridingsolo.component';
 
 const appRoutes: Routes = [
   {
