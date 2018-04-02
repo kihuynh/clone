71b24001319b689e8dfec7a470f73f4c326ac670
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-02 12:34:35 -0700

    import and route beauty component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,15 +2,20 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { RidingsoloComponent } from './ridingsolo/ridingsolo.component';
+import { BeautyComponent } from './beauty/beauty.component';
 
 const appRoutes: Routes = [
   {
     path:'',
-    component: MainComponent,
+    component: MainComponent
   },
   {
     path: 'ridingsolo',
     component: RidingsoloComponent
+  },
+  {
+    path: 'beauty',
+    component: BeautyComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
