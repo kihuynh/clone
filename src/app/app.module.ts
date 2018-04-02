d54f2052ce96f9408f86fe72aa7bda5358bc42f3
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-02 13:10:58 -0700

    add all of the created components into module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,28 @@ import { AppComponent } from './app.component';
 import { RidingsoloComponent } from './ridingsolo/ridingsolo.component';
 import { MainComponent } from './main/main.component';
 import { routing } from './app.routing';
+import { BeautyComponent } from './beauty/beauty.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { FoodComponent } from './food/food.component';
+import { HomeComponent } from './home/home.component';
+import { IssuesComponent } from './issues/issues.component';
+import { MoreComponent } from './more/more.component';
+import { VideoComponent } from './video/video.component';
+import { ClassesComponent } from './classes/classes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RidingsoloComponent,
-    MainComponent
+    MainComponent,
+    BeautyComponent,
+    FashionComponent,
+    FoodComponent,
+    HomeComponent,
+    IssuesComponent,
+    MoreComponent,
+    VideoComponent,
+    ClassesComponent
   ],
   imports: [
     BrowserModule,
