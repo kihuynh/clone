65024bc8e1478c5cd478f53c7b6aa759e000bbc9
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-06 13:39:34 -0700

    add firebase route to main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  providers: [ArticleService]
 })
-export class MainComponent implements OnInit {
 
-  constructor() { }
+export class MainComponent implements OnInit {
+  articles: FirebaseListObservable<any[]>;
+
+  constructor(
+    private router: Router,
+    private articleService: ArticleService) { }
 
   ngOnInit() {
+    this.articles = this.articleService.getArticles();
   }
 
 }
