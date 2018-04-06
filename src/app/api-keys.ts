b62a650a6659a6f53e8d11c0e7116009f64b7d0a
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-06 12:37:35 -0700

    add api key to file to prep for firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyAM3Nu8S31_3V0ogp0UxeNYti3ys-2gDYc",
+  authDomain: "siterebuild-a7b04.firebaseapp.com",
+  databaseURL: "https://siterebuild-a7b04.firebaseio.com",
+  projectId: "siterebuild-a7b04",
+  storageBucket: "siterebuild-a7b04.appspot.com",
+  messagingSenderId: "355846211424"
+  };
