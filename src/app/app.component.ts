eaf1ed57d2f8c1dee61970e80b5f4643a7c71766
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-06 13:35:47 -0700

    remove items in this file. misplaced in wrong one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Article } from './models/article.model';
+import { Router } from '@angular/router';
+import { routing } from './app.routing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Brit & Co';
+
+  articles = Article;
+
+  constructor (
+    ) { }
 }
