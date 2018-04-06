a72c4a870064d18872fb4f6ebdae53754808f963
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-06 13:17:48 -0700

    import and add firebase to service file

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Article } from './models/article.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Injectable()
 export class ArticleService {
+  articles: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.articles = database.list('articles');
+  }
+
+  getArticles() {
+   return this.articles;
+ }
 
 }
