b77593825e1fa5a1585464b3a02abcb75b5dd8c3
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-04-08 11:38:45 -0700

    add firebase route to ridingsolo compo

--- src/app/ridingsolo/ridingsolo.component.ts
+++ src/app/ridingsolo/ridingsolo.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-ridingsolo',
   templateUrl: './ridingsolo.component.html',
-  styleUrls: ['./ridingsolo.component.css']
+  styleUrls: ['./ridingsolo.component.css'],
+  providers: [ArticleService]
 })
 export class RidingsoloComponent implements OnInit {
+  articles: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private articleService: ArticleService) { }
 
   ngOnInit() {
+    this.articles= this.articleService.getArticles();
   }
 
 }
